95d4bb0dbd4f9346c787b0bbf9db8564583031a3
anjithmathew <167713334+anjithU@users.noreply.github.com>
2025-10-08 20:39:29 +0200

    leap year checker

---
+++ leap_year.py
@@ -0,0 +1,15 @@
+def is_leap_year(year):
+    
+    if year % 4 == 0:
+        if year % 100 !=0:
+            return True
+        elif year %400 == 0:
+            return True
+        else:
+            return False
+    else:
+        return False
+    # Write your code here. 
+    # Don't change the function name.
+    
+is_leap_year(2024)
